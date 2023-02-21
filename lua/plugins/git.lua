874331535460b1a450fc65a19a1b024beb861d75
uniful <inferdawn@gmail.com>
2023-02-21 15:59:56 +0800

    优化

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -11,7 +11,7 @@ return {
     -- neovim的magit
     {
         "TimUntersberger/neogit",
-        event = "VeryLazy",
+        cmd = "Neogit",
         opts = {}
     },
     -- 环浏览任何 git 版本的所有修改文件的差异

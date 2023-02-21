8cd5f16c615b0483c13ed3f53b33b578f2b146ff
uniful <inferdawn@gmail.com>
2023-02-21 16:00:11 +0800

    优化

--- lua/plugins/picker.lua
+++ lua/plugins/picker.lua
@@ -2,7 +2,7 @@ return {
     -- 图标选择器
     {
         "ziontee113/icon-picker.nvim",
-        event = "BufReadPost",
+        cmd = {"IconPickerNormal","IconPickerYank","IconPickerInsert"},
         opts = {
             disable_legacy_commands =true,
         }

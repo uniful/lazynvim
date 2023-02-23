38df75f85e5048cab1c08e51bd6001d450c7fad7
uniful <inferdawn@gmail.com>
2023-02-23 19:46:10 +0800

    优化

--- lua/plugins/langext.lua
+++ lua/plugins/langext.lua
@@ -3,13 +3,13 @@ return {
     -- C/C++
     {
         "p00f/clangd_extensions.nvim",
-        ft = {"c","cpp","cxx","C","cc","c++","h","H","hh","hpp","hxx"},
+        ft = {"c","cpp","cxx","C","cc","c++","cp","h","H","hh","hpp","hxx","h++","hp"},
         opts = {}
     },
     -- markdown
     {
         "mickael-menu/zk-nvim",
-        ft = "md",
+        ft = {"md","markdown"},
         opts = {
             picker = "telescope",
         }

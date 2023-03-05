7e20dd0140b5d17eda053a850442c44e7787c7bd
uniful <inferdawn@gmail.com>
2023-03-05 19:32:31 +0800

    简单配置nvim-jdtls插件

--- lua/ftplugin/java.lua
+++ lua/ftplugin/java.lua
@@ -1,25 +1,5 @@
 local config = {
-    cmd = {
-        "java",
-            '-Declipse.application=org.eclipse.jdt.ls.core.id1',
-        '-Dosgi.bundles.defaultStartLevel=4',
-        '-Declipse.product=org.eclipse.jdt.ls.core.product',
-        '-Dlog.protocol=true',
-        '-Dlog.level=ALL',
-        '-Xms1g',
-        '--add-modules=ALL-SYSTEM',
-        '--add-opens', 'java.base/java.util=ALL-UNNAMED',
-        '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
-        '-jar','/home/colin/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar',
-        '-configuration','/home/colin/.local/share/nvim/mason/packages/jdtls/config_linux',
-        '-data','/home/colin/.cache/jdtls/workspace/jdt.ls-java-project/',
-        root_dir = require("jdtls.setup").find_root({".git", "mvnw", "gradlew"}),
-        settings = {
-            java = {}
-        },
-        init_options = {
-            bundles = {}
-        }
-    }
+    cmd = {"/home/colin/.local/share/nvim/mason/bin/jdtls"},
+    root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
 }
 require("jdtls").start_or_attach(config)
